c317d9fcfdac5618a4ccc760bb5b31b8ff26ee6c
fedPass <federica.passanante@gmail.com>
2019-11-27 17:20:49 +0100

    aggiunte funzioni da completare

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
-    
+    $('.next').click(function(){
+        var imgCurrent = $('img.active');
+        var bulletCurrent = $('i.active');
+        var imgNext = imgCurrent.next('img');
+        var bulletNext = bulletCurrent.next('i');
+        if (imgNext.length == 0) {
+            imgNext = $('.img-container img:first-child');
+            bulletNext = $('bullet-container i:first-child');
+        }
+        imgCurrent.removeClass('active');
+        bulletCurrent.removeClass('active');
+        imgNext.addClass('active');
+        bulletNext.addClass('active');
+    });
+    $('.back').click(function(){
+        var imgCurrent = $('img.active');
+        var bulletCurrent = $('i.active');
+        var imgPrev = imgCurrent.prev('img');
+        var bulletPrev = bulletCurrent.prev('i');
+        if (imgPrev.length == 0) {
+            imgPrev = $('.img-container img:last-child');
+            bulletPrev = $('bullet-container i:last-child');
+        }
+        imgCurrent.removeClass('active');
+        bulletCurrent.removeClass('active');
+        imgPrev.addClass('active');
+        bulletPrev.addClass('active');
+
+    });
 })
