be145d501b9316b727ac0e5a267d0153afb9de4d
fedPass <federica.passanante@gmail.com>
2019-11-27 19:48:20 +0100

    prove per bonus

--- main.js
+++ main.js
@@ -27,4 +27,31 @@ $(document).ready(function(){
         imgPrev.addClass('active');
         bulletPrev.addClass('active');
     });
+
+    //Bonus: cliccando sul pallino si apre img corrispondende
+    $('.fas.fa-circle').click(function(){
+        //devo prendere l'i del pallino
+        var indexBullet = $(this).index();
+        console.log('hai cliccato sul pallino in posizione: ' + indexBullet);
+        // var bulletCurrent = $('i.active');
+        // bulletCurrent.removeClass('active');
+        // $(this).addClass('active');
+
+        //deve diventare active la img con i uguale a indexBullet
+        // var fotoCorrispondente = $('img[indexBullet]');
+        // $('img[indexBullet]').addClass('active');
+
+        // //dovrei rimuovere active da quella attuale per agg a quello in posizione indexBullet
+        // var imgCurrent = $('img.active');
+        // var bulletCurrent = $('i.active');
+        // imgCurrent.removeClass('active');
+        // bulletCurrent.removeClass('active');
+        //
+        // //prendi in div con le img --> $('.img-container')
+        // //trova img in posizione (indexBullet)
+        // //aggiungigli la classe active --> .addClass('active');
+        // // $('.img-container').find('img[indexBullet]').addClass('active');
+        // $('.img-container').children([img[indexBullet]).addClass('active');
+        // $('.bullet-container').children([i[indexBullet]).addClass('active');
+    });
 })
