401a10e7f8df5e0f2e8625d252f3bb5011e63c83
fedPass <federica.passanante@gmail.com>
2019-11-27 17:26:50 +0100

    click su back e next e bullet progression

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $('.next').click(function(){
         var imgCurrent = $('img.active');
-        var bulletCurrent = $('i.active');
         var imgNext = imgCurrent.next('img');
+        var bulletCurrent = $('i.active');
         var bulletNext = bulletCurrent.next('i');
         if (imgNext.length == 0) {
             imgNext = $('.img-container img:first-child');
-            bulletNext = $('bullet-container i:first-child');
+            bulletNext = $('.bullet-container i:first-child');
         }
         imgCurrent.removeClass('active');
         bulletCurrent.removeClass('active');
@@ -15,17 +15,16 @@ $(document).ready(function(){
     });
     $('.back').click(function(){
         var imgCurrent = $('img.active');
-        var bulletCurrent = $('i.active');
         var imgPrev = imgCurrent.prev('img');
+        var bulletCurrent = $('i.active');
         var bulletPrev = bulletCurrent.prev('i');
         if (imgPrev.length == 0) {
             imgPrev = $('.img-container img:last-child');
-            bulletPrev = $('bullet-container i:last-child');
+            bulletPrev = $('.bullet-container i:last-child');
         }
         imgCurrent.removeClass('active');
         bulletCurrent.removeClass('active');
         imgPrev.addClass('active');
         bulletPrev.addClass('active');
-
     });
 })
